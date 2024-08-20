f1ce553621294c15e00fddc3abd6f3256b4140c7
MalikMohsin5885 <malikmohsin8239@gmail.com>
2024-08-20 21:18:52 +0500

    main.ts changed

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS directly with NestJS
   app.enableCors({
-    origin: 'https://mohsinrasheed.vercel.app/', // Removed trailing slash
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: '*', // You can restrict this to specific origins
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  // Handle preflight requests
+  // app.use((req, res, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.sendStatus(204);
+  //   } else {
+  //     next();
+  //   }
+  // });
+
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
